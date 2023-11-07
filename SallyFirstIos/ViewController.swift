98d39301f2029ff08cd8db6ae0522a1a7f8dce6e
sally <salo.vashadze@gmail.com>
2023-11-07 17:01:15 +0400

    Initial Commit

---
+++ SallyFirstIos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SallyFirstIos
+//
+//  Created by Salome Vashadze on 07.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
